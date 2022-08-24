f0dd8a5172025763226e19de480dcca0a16670e1
hikaRU-135 <hikari.desu13@gmail.com>
2022-08-24 17:11:02 +0400

    dev authtorization

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
+import { observer } from 'mobx-react-lite';
 import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import AppRouter from './components/AppRouter';
+import {useContext, useState, useEffect} from "react";
+import { Context } from './index.js';
+import { Spinner } from 'react-bootstrap'; 
+import { check } from './http/userApi';
 
+const App = observer(() => {
+   
 
-
-function App() {
   return (
     <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
   );
-}
+})
 
 export default App;
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,27 +1,37 @@
 import { observer } from 'mobx-react-lite';
 import React, { useContext, useState } from 'react'
-import { Card, Row, NavLink, Col, Button, Form, Container } from 'react-bootstrap';
-import { useLocation } from 'react-router-dom';
+import { Card, Row, Nav, Col, Button, Form, Container } from 'react-bootstrap';
+import { useLocation, useNavigate } from 'react-router-dom';
 import NavBarAuth from '../components/NavBarAuth';
 import { Context } from "../index";
 import {login, registration} from "../http/userApi.js";
-import {REGISTRATION_ROUTE, LOGIN_ROUTE} from "../utils/consts";
+import {REGISTRATION_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 
 const Auth = observer(() => {
-  const {user} = useContext(Context);
-  const location = useLocation();
-  const isLogin  = location.pathname === LOGIN_ROUTE;
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+    const {user} = useContext(Context)
+    const location = useLocation()
+    const history = useNavigate()
+    const isLogin = location.pathname === LOGIN_ROUTE
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-  const click = async() => {
-    if (isLogin) {
-      const response = await login();
-    }else{
-      const response = await registration(email, password);
+    
+    const click = async () => {
+        try {
+            let data;
+            if (isLogin) {
+                data = await login(email, password);
+            } else {
+                data = await registration(email, password);
+            }
+            user.setUser({data});
+            user.setIsAuth(true);
+            history(SHOP_ROUTE);
+        } catch (e) {
+            console.log(e);
+        }
+
     }
-  }
-
   
   return (
     <div>
@@ -48,17 +58,17 @@ const Auth = observer(() => {
 
 
               <Row className="d-flex">
-                <Button variant={"outline-success"} >
+                <Button variant={"outline-success"} onClick={click}>
                       {isLogin ? 'Вход' : 'Регистрация'}          
                 </Button>
 
                 {isLogin ? 
                 <div className="d-flex flex-row justify-content-around">
-                  <Col md={2} offset={5}><NavLink to={REGISTRATION_ROUTE}>Регистрация</NavLink></Col>
+                  <Col md={2} offset={5}><Nav.Link href={REGISTRATION_ROUTE}>Регистрация</Nav.Link></Col>
                   <Col md={2} offset={3}><p>Забыли пароль?</p></Col>
                 </div> : 
                 <div>
-                  <Col mg={2}><NavLink to={LOGIN_ROUTE}>Вход</NavLink></Col>
+                  <Col mg={2}><Nav.Link href={LOGIN_ROUTE}>Вход</Nav.Link></Col>
                 </div>}
 
               </Row>   
